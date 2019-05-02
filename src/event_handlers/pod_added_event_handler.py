9bfc892755f01466ff94813ec147dbc447e31cd8
Satish Kumar <satish.kumar@kasasa.com>
2019-05-02 01:09:56 -0500

    Add logging.

--- src/event_handlers/pod_added_event_handler.py
+++ src/event_handlers/pod_added_event_handler.py
@@ -1,12 +1,19 @@
 import pods
+import logging
+
+logger = logging.getLogger(__name__)
 
 class PodAddedEventHandler(object):
     def __init__(self):
         self.__pods = pods.Pods()
 
     def handle(self, pod):
+        logger.info("Handle the pod ADDED event.")
         pod_role = self.__pods.get_pod_role(pod)
         if not pod_role:
+            logger.info("Pod does not have a pod role.")
             return
 
+        logger.info(f"Pod has pod role {pod_role}.")
         self.__pods.ensure_ec2_metadata(pod)
+        logger.info("Handled the pod ADDED event.")
